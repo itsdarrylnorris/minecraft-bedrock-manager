7fab01c43fd75d290eb02920e2745fc17ab52362
Darryl Norris <admin@darrylnorris.com>
2021-02-07 01:00:58 -0800

    Changing the path of the logs

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 echo "Starting deployment...🚀";
 cd ~/Projects/minecraft-manager;
 echo "Pulling code...🔥"
+
+# TODO: Adding support to run yarn? To check dependencies?
 git pull;
 echo "Deployment complete...😚"
 
