94efc23f62d4e692f92ce2016c2e406ae61955e9
Mandy Hom <amandakhom@gmail.com>
2021-10-21 19:47:42 -0700

    Testing cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
+import { executeShellScript } from './utils'
 
 // Grab provided args.
 const [, , ...args] = process.argv
 
-// Print hello world provided args
-// console.log(`Hello World ${args}`)
+executeShellScript(`node /root/Projects/minecraft-manager/dist/index.js ${args}`)
